3caaa05b760f085d711ed66461f5b023bb895204
furryfresh24 <furryfresh.app@gmail.com>
2025-04-22 22:10:55 +0800

    Pushing

--- app/screens/profile/settings.tsx
+++ app/screens/profile/settings.tsx
@@ -2,6 +2,9 @@ import React, { useLayoutEffect } from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import { useNavigation } from 'expo-router';
 import dimensions from "../../utils/sizing";
+import MainContPlain from '../../components/general/background_plain';
+import Spacer from '../../components/general/spacer';
+import { ChevronRight } from "lucide-react-native"; // Import a specific icon
 
 interface SettingOption {
   id: string;
@@ -10,12 +13,12 @@ interface SettingOption {
 }
 
 const SETTINGS_OPTIONS: SettingOption[] = [
-  { id: '1', title: 'Account', onPress: () => {} },
-  { id: '2', title: 'Privacy', onPress: () => {} },
-  { id: '3', title: 'Security & permissions', onPress: () => {} },
-  { id: '4', title: 'Your orders', onPress: () => {} },
-  { id: '5', title: 'Share profile', onPress: () => {} },
-  { id: '6', title: 'Log Out', onPress: () => {} },
+  { id: '1', title: 'Account', onPress: () => { } },
+  { id: '2', title: 'Privacy', onPress: () => { } },
+  { id: '3', title: 'Security & permissions', onPress: () => { } },
+  { id: '4', title: 'Your orders', onPress: () => { } },
+  { id: '5', title: 'Share profile', onPress: () => { } },
+  { id: '6', title: 'Log Out', onPress: () => { } },
 ];
 
 const Settings: React.FC = () => {
@@ -28,11 +31,12 @@ const Settings: React.FC = () => {
       headerTintColor: 'black',
     });
   }, [navigation]);
-  
+
 
   const renderItem = ({ item }: { item: SettingOption }) => (
     <TouchableOpacity style={styles.item} onPress={item.onPress}>
       <Text style={styles.itemText}>{item.title}</Text>
+      <ChevronRight size={dimensions.screenWidth * 0.05} color="#000" style={styles.chevronIcon} />
     </TouchableOpacity>
   );
 
@@ -41,15 +45,32 @@ const Settings: React.FC = () => {
   );
 
   return (
-    <View style={styles.container}>
-      {renderSectionHeader('Account')}
-      <FlatList
-        data={SETTINGS_OPTIONS}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        scrollEnabled={false}
-      />
-    </View>
+    <MainContPlain>
+      <Spacer height={dimensions.screenHeight * 0.023} />
+      <View style={styles.container}>
+        {renderSectionHeader('Account')}
+        <View style={styles.listCont}>
+          <FlatList
+            data={SETTINGS_OPTIONS}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            scrollEnabled={false}
+          />
+        </View>
+      </View>
+      <Spacer height={dimensions.screenHeight * 0.023} />
+      <View style={styles.container}>
+        {renderSectionHeader('Account')}
+        <View style={styles.listCont}>
+          <FlatList
+            data={SETTINGS_OPTIONS}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            scrollEnabled={false}
+          />
+        </View>
+      </View>
+    </MainContPlain>
   );
 };
 
@@ -59,25 +80,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: dimensions.screenWidth * 0.05,
-    backgroundColor: 'white',
+    paddingTop: 0
   },
   title: {
     fontSize: dimensions.screenWidth * 0.06,
     fontWeight: 'bold',
     marginBottom: dimensions.screenHeight * 0.02,
   },
+  listCont: {
+    backgroundColor: 'white',
+    borderRadius: 6
+  },
   sectionHeader: {
     fontSize: dimensions.screenWidth * 0.045,
-    fontWeight: 'bold',
-    marginTop: dimensions.screenHeight * 0.03,
+    fontFamily: 'Poppins-SemiBold',
     marginBottom: dimensions.screenHeight * 0.01,
   },
   item: {
     padding: dimensions.screenHeight * 0.02,
-    borderBottomWidth: 1,
-    borderBottomColor: '#e0e0e0',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  chevronIcon: {
+    marginLeft: 10,
   },
   itemText: {
-    fontSize: dimensions.screenWidth * 0.045,
+    fontSize: dimensions.screenWidth * 0.04,
+    fontFamily: 'Poppins-Regular'
   },
 });
